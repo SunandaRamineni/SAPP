da77cfadb568f0c86bf9e7bb89dbc6c22a3dc333
Ramineni Sunanda <sramineni@gainsight.com>
2020-06-10 20:07:25 +0530

    checking danger version

--- tests.sh
+++ tests.sh
@@ -4,4 +4,4 @@
 /usr/bin/bundle --version
 /usr/bin/bundle install
 gem install danger-xcov
-/usr/bin/bundle exec /usr/local/bin/danger --fail-on-errors=true
+/usr/bin/bundle exec /usr/local/bin/danger --version
