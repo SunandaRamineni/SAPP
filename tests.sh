daaea279866d5ce52d61f0caadb12b7a0fa9b3e4
Ramineni Sunanda <sramineni@gainsight.com>
2020-06-10 20:40:09 +0530

    added fastlane tests to script

--- tests.sh
+++ tests.sh
@@ -3,5 +3,5 @@
 #bundle exec fastlane tests
 /usr/bin/bundle --version
 /usr/bin/bundle install
-gem install danger-xcov
-/usr/bin/bundle exec /usr/local/bin/danger --version
+fastlane tests
+/usr/bin/bundle exec /usr/local/bin/danger --fail-on-errors=true
